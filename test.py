0829ccb00470fe9d83c1fa7ca7f18e39f1f2dfb7
korwin <levozavr@mail.ru>
2018-10-02 11:48:07 +0300

    small fixes

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from Parsers.csv_parser import CsvParser
 
-a = CsvParser(filename="/index/avg_index.csv")
+a = CsvParser(filename="./index/avg_index.csv")
 a.open()
-print(a.get())
+print(a.get_data())
 a.close()
