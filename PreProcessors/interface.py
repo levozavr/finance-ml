1af1fec1c4b3a1a998badaf9080c0c93e4fcb4f5
korwin <levozavr@mail.ru>
2018-10-02 13:55:57 +0300

    add interface for preprocessor

---
+++ PreProcessors/interface.py
@@ -0,0 +1,18 @@
+from interface import Interface
+
+
+class PreProcessorInterface(Interface):
+    def __init__(self, filename, **kwargs):
+        pass
+
+    def start(self, ws_pred=20, ws_future=7):
+        pass
+
+    def get_train(self):
+        pass
+
+    def get_test(self):
+        pass
+
+    def get_all_data(self):
+        pass
