fe5e95bcdfb4172e89437ca66c69b1734c1fa498
korwin <levozavr@mail.ru>
2019-02-01 22:51:45 +0300

    some changes

--- ml_model/cfar_model.py
+++ ml_model/cfar_model.py
@@ -2,21 +2,21 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from PreProcessors.csv_pre_processor_heatmap import PreProcessor
+from PreProcessors.csv_pre_processor_inctiments import PreProcessor
 import matplotlib.pyplot as plt
 import os
 
 
 batch_size = 200
-num_classes = 3
-epochs = 20
+num_classes = 2
+epochs = 10
 data_augmentation = True
 num_predictions = 20
 save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'keras_cifar10_trained_model.h5'
 
-PP = PreProcessor(filename="../index/avg_index.csv")
-PP.start(grade=20)
+PP = PreProcessor(filename="../index/FX_EURKRW.csv")
+PP.start()
 """
 train_x: обучающие данные содержащие в себе heatmap 20X20
 train_y: обучающие данные(предсказания) содержащие в себе 7-дневные тренды"""
@@ -33,29 +33,29 @@ print(x_test.shape[0], 'test samples')
 
 
 model = Sequential()
-model.add(Conv2D(64, (5, 5), padding='same',
-                 input_shape=(20, 20, 1)))
+model.add(Conv2D(128, (5, 5), padding='same',
+                 input_shape=(25, 25, 1)))
 model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
+model.add(Conv2D(64, (5, 5)))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(Dropout(0.75))
 
 model.add(Conv2D(64, (3, 3), padding='same'))
 model.add(Activation('relu'))
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Dropout(0.75))
 
 model.add(Flatten())
-model.add(Dense(512))
+model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(3, activation='softmax'))
+model.add(Dropout(0.75))
+model.add(Dense(2, activation='softmax'))
 
 # initiate RMSprop optimizer
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+opt = keras.optimizers.rmsprop(lr=0.01, decay=1e-6)
 
 # Let's train the model using RMSprop
 if __name__ == "__main__":
@@ -66,7 +66,8 @@ if __name__ == "__main__":
     """
     history: данные полученные во время обучения сети, необходимые для построения различных графиков"""
 
-    history = model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=epochs)
+    history = model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=epochs,
+                        shuffle=True, validation_data=(x_test, y_test))
     """if not os.path.isdir(save_dir):
         os.makedirs(save_dir)
     model_path = os.path.join(save_dir, model_name)
@@ -78,6 +79,16 @@ if __name__ == "__main__":
 
     scores = model.evaluate(x_test, y_test, verbose=1)
     print("Точность работы на тестовых данных: %.2f%%" % (scores[1]*100))
+    ans = model.predict(x_test, verbose=1)
+    q = 0
+    w = 0
+    for a, b in zip(ans,y_test):
+        if a[1] > a[0] and b[1] == 1:
+            q+=1
+        if b[1] == 1:
+            w+=1
+
+    print(q/w)
     plt.plot(history.history['acc'])
     plt.show()
 
--- test.py
+++ test.py
@@ -1,20 +1,26 @@
-from PreProcessors.csv_pre_processor_layer_heatmap import PreProcessor
+from PreProcessors.csv_pre_processor_inctiments import PreProcessor
 from Parsers.csv_parser_FX import Parser
 import logging
 logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
                     level=logging.DEBUG, filename='test.log')
 logging.info('program start')
-a = PreProcessor(["./index/FX_EURKRW.csv","./index/FX_JPYKRW.csv","./index/FX_CNYKRW.csv"])
-a.start(grade=8)
-x, y = a.get_all_data()
-
+a = PreProcessor("./index/FX_EURKRW.csv")
+a.start()
+q = a.delta
+z = sum(q)/len(q)
+v = 0
+for i in q:
+    v+=(i-z)**2
+v/=(len(q))
+import math
+print(math.sqrt(v))
+x, y = a.get_test()
+print(len(y))
 a0, a1, a2 = 0, 0, 0
 for i in y:
-    if i == 0:
-        a0 += 1
-    if i == 1:
+    if i[0] >i[1]:
         a1 += 1
-    if i == 2:
-        a2 += 1
+    if i[0] < i[1]:
+        a0 += 1
 print(a0, a1, a2)
 logging.info('program stopped')
